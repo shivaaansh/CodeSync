c7cc741baa3569bd867eb19402fedc3c0942a26f
Shivansh Gupta <122895124+shivaaansh@users.noreply.github.com>
2025-12-19 14:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (14.70%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int n = 0, m = b.size();
-        string s = "";
-        int cnt = 0;
-        while(n <= (m + a.size())) {
-            s += a;
-            cnt += 1;
-            n = s.size();
-            for(int i = 0; i < n; i++) {
-                bool flag = false;
-                if(s[i] == b[0] && (i+m-1) < n) {    
-                    flag = true;
-                    for(int j = 0; j < m; j++) {
-                        if(s[i+j] != b[j]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(flag == true) {
-                        return cnt;
-                    }
-                }
+    bool solution(string &a, string &b) {
+        string x = b + "#" + a;
+        int n = x.size();
+
+        vector<int> lps(n, 0);
+
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];
+
+            while (j > 0 && x[i] != x[j]) {
+                j = lps[j - 1];
+            }
+
+            if (x[i] == x[j]) {
+                j++;
+            }
+
+            lps[i] = j;
+
+            if (lps[i] == b.size()) {
+                return true;
             }
         }
+        return false;
+    }
+
+    int repeatedStringMatch(string a, string b) {
+        string tmp = a;
+        int ans = 1;
+
+        while (a.size() < b.size()) {
+            a += tmp;
+            ans++;
+        }
+
+        if (solution(a, b)) return ans;
+
+        a += tmp;
+        if (solution(a, b)) return ans + 1;
+
         return -1;
     }
 };
