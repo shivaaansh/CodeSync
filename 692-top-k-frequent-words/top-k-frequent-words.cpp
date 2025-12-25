aa10a72726c22e4cd5b2af9891b5f933292bb9bd
Shivansh Gupta <122895124+shivaaansh@users.noreply.github.com>
2025-12-25 13:59:17 +0530

    Time: 4 ms (44.78%) | Memory: 18.4 MB (9.21%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,31 @@
+struct logic {
+    bool operator()(pair<int,string>& a, pair<int,string>& b) {
+        if(a.first == b.first) {
+            return a.second < b.second;
+        }return a.first > b.first;
+    }
+};
+class Solution {
+public:
+    #define pp pair<int,string>
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        priority_queue<pp, vector<pp>, logic> pq;
+        unordered_map<string, int> mp;
+        for(string s : words) mp[s]++;
+        for(auto it : mp) {
+            
+            pq.push({it.second, it.first});
+            
+        }
+        vector<string> ans;
+        while(pq.size() > k) {
+            pq.pop();
+        }
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
