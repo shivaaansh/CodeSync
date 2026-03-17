78913475470a7f81854ca93426595cafc763ee58
Shivansh Gupta <122895124+shivaaansh@users.noreply.github.com>
2026-03-17 19:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (63.53%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int dp[1001][1002][2];
-    int f(int i, int prev, int b, vector<int>& nums) {
-        if(i < 0) return 0;
-        if(dp[i][prev+1][b] != -1) {
-            return dp[i][prev+1][b];
-        }
-        int ntake = f(i-1, prev, b, nums);
-        int take = 0;
-        if(prev == -1)  
-            take = 1 + f(i-1, i, b, nums);
-        else if(nums[i] != nums[prev] && (nums[i] - nums[prev] > 0) == b) {
-            take = 1 + f(i-1, i, !b, nums);
-        }
-        return dp[i][prev+1][b] = max(take, ntake);
-    }
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        
-        return max(f(n-1, -1, 0, nums), f(n-1, -1, 1, nums));
+        if(n == 1) return 1;
+        int i = 0;
+        while((i + 1 < n) && nums[i] == nums[i+1]) ++i;
+        if(i == n-1) return 1;
+        bool increasing = nums[i+1] > nums[i];
+        int ans = 2;
+        for(;i + 1 < n; ++i){
+            if(increasing && nums[i+1] < nums[i]){
+                increasing = false;
+                ++ans;
+            }
+            if(!increasing && nums[i+1] > nums[i]){
+                increasing  = true;
+                ++ans;
+            }
+        }
+        return ans;
     }
 };
